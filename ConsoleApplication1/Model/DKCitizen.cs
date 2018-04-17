58449aea289e4769921d3f6a9046a829c86d4d78
duckdyhr <duckdyhr@hotmail.com>
2018-04-17 12:45:07 +0200

    Projects Service and Model created

---
+++ ConsoleApplication1/Model/DKCitizen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Model
+{
+    [Serializable()]
+    [XmlRoot("Citizen")]
+    public class DKcitizen
+    {
+        string firstName { get; set; }
+        string surName { get; set; }
+        string cprNr { get; set; }
+        string Adress1 { get; set; }
+        string Adress2 { get; set; }
+        string postalCode { get; set; }
+        string city { get; set; }
+        string maritalStatus { get; set; }
+        string spouseCPR { get; set; }
+        List<string> childrenCPR { get; set; }
+        List<string> parentsCPR { get; set; }
+        string doctorCVR { get; set; }
+    }
+}
---
+++ ConsoleApplication1/Model/EUcitizen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Model
+{
+    [Serializable()]
+    [XmlRoot("Citizen")]
+    public class EUcitizen
+    {
+        public string euccid { get; set; }
+        public string christianName { get; set; }
+        public string familyName { get; set; }
+        public string gender { get; set; }
+        public string streetAndHouseNo { get; set; }
+        public string apartmentNo { get; set; }
+        public string county { get; set; }
+        public string city { get; set; }
+        public string birthCountry { get; set; }
+        public string currentCountry { get; set; }
+    }
+}
